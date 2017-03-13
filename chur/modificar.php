36efd7dd77bcff89f134fa9e266988352bcb0db5
Miguel Perez <perezvz.miguel@gmail.com>
2017-03-12 20:26:54 -0600

    modificando el inicio de admin con tabla

--- chur/modificar.php
+++ chur/modificar.php
@@ -14,34 +14,15 @@
       <img src="images/perfil.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity">
     </a>
     <span class="w3-opennav w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
-    <h4><b>Servicio Hidraulico</b></h4>
-    <h5><b>La Fuerza</b></h5>
+    <h1><b>Servicio Hidraulico</b></h1>
+    <h3><b>La Fuerza</b></h3>
+
     <div class="w3-section w3-bottombar w3-padding-16">      
     </div>
   </header>
   
 
   <!-- First Photo Grid-->
-  <div class="w3-responsive">
-    <table class="w3-table-all w3-centered w3-hoverable">
-      <tr class="w3-red">
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Points</th>
-      </tr>
-      <tr class="w3-hover-green">
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-      </tr>
-      <tr class="w3-hover-green">
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-      </tr>
-      </table>
-  </div>
-  <br>
   <div class="w3-row-padding">
     <div class="w3-third w3-container w3-margin-bottom">
       <img src="/w3images/mountains.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
@@ -59,7 +40,7 @@
     </div>
     <div class="w3-third w3-container">
       <img src="/w3images/nature.jpg" alt="Norway" style="width:100%" class="w3-hover-opacity">
-      <div class="w3-table-all w3-white">
+      <div class="w3-container w3-white">
         <p><b>Lorem Ipsum</b></p>
         <p>Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
       </div>
@@ -80,6 +61,42 @@
   </div>
 
   
+  
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-large w3-grey">
+    <h4 id="contact"><b>Contact Me</b></h4>
+    <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
+        <p>email@email.com</p>
+      </div>
+      <div class="w3-third w3-teal">
+        <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
+        <p>Chicago, US</p>
+      </div>
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
+        <p>512312311</p>
+      </div>
+    </div>
+    <hr class="w3-opacity">
+    <form action="/action_page.php" target="_blank">
+      <div class="w3-group">
+        <label>Name</label>
+        <input class="w3-input w3-border" type="text" name="Name" required>
+      </div>
+      <div class="w3-group">
+        <label>Email</label>
+        <input class="w3-input w3-border" type="text" name="Email" required>
+      </div>
+      <div class="w3-group">
+        <label>Message</label>
+        <input class="w3-input w3-border" type="text" name="Message" required>
+      </div>
+      <button type="submit" class="w3-button w3-black w3-padding-large w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
+    </form>
+  </div>
+
   <!-- Footer -->
   <footer class="w3-container w3-padding-32 w3-dark-grey">
   <div class="w3-row-padding">
